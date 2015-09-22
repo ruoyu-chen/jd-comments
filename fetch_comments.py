43c2ef798f3dd12f93154622452b13112ec99e54
0neSe7en <siyuan.se7en@gmail.com>
2015-09-22 08:30:21 +0800

    resume from current progress. if time out, retry after 3 minutes

--- fetch_comments.py
+++ fetch_comments.py
@@ -4,15 +4,29 @@ import time
 import random
 
 base_url = r'http://s.club.jd.com/productpage/p-%s-s-0-t-0-p-%d.html'
-results = open('skuid_comments.json', mode='w')
+results = open('skuid_comments.json', mode='a')
 skuid_file = open('skuids.txt', mode='r')
 user_agents_file = open('uas.txt', mode='r')
+current_progress = open('progress', mode='r')
+progress = current_progress.read()
+current_skuid = None
+current_page = None
+if progress:
+    current_skuid = progress.strip().split(' ')[0].strip()
+    current_page = progress.strip().split(' ')[1].strip()
 ua_list = [x.strip() for x in user_agents_file.readlines()]
 
 for skuid_str in skuid_file.readlines():
+    if current_skuid:
+        if skuid_str.strip() != current_skuid:
+            continue
+        page = int(current_page)
+        current_skuid = None
+        current_page = None
+    else:
+        page = 0
     skuid = skuid_str.strip()
     print('Current Skuid:', skuid)
-    page = 0
     while True:
         sec = random.randint(1, 4)
         time.sleep(sec)
@@ -23,7 +37,8 @@ for skuid_str in skuid_file.readlines():
         except:
             with open('progress', mode='w') as p:
                 p.write(skuid + ' ' + str(page))
-                exit(0)
+            time.sleep(180)
+            continue
         if not comments_json.text:
             break
         comments = json.loads(comments_json.text)
